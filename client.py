07e9205bc2706e462c3bc378a82bb423bac04c56
WakeUpPL <138322442+WakeUpPL@users.noreply.github.com>
2023-11-19 16:36:31 +0100

    new update

--- client.py
+++ client.py
@@ -4,15 +4,25 @@ sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY, 1)
 sock.connect(('localhost',10000))
 
+old_data=(0)
+
 while True:
     #read enter msg
+    print ('opent reiler door?')
+    td=input ()
 
     #send msg on server
-    sock.send('Check #1, Check #2, Check #3, Check #4'.encode())
+    sock.send(td.encode())
 
     #takeing msg from server
     data=sock.recv(1024)
     data=data.decode()
-    print(data)
+    if data!=old_data:
+        old_data=data
+        print(data)
+    else:
+        pass
+    
+
 
     
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import socket
 import time
 
+class User():
+    def __init__(self,conn,addr,err):
+        self.conn=conn
+        self.addr=addr
+        self.err=err
+
 main_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY, 1)
 main_socket.bind(('localhost',10000))
@@ -8,21 +14,22 @@ main_socket.setblocking(0)
 main_socket.listen(5)
 print('Create sockets')
 
-users_socket=[]
+users=[]
 while True:
     #check/add new connection
     try:
         new_socket,addr=main_socket.accept()
         print('Connection ',addr)
         new_socket.setblocking(0)
-        users_socket.append(new_socket)
+        new_user=User(new_socket, addr, 0)
+        users.append(new_user)
     except:
-        print('No connections users')
+        #print('No connections users')
         pass
     #read msg
-    for sock in users_socket:
+    for user in users:
         try:
-            data=sock.recv(1024)
+            data=user.conn.recv(1024)
             data=data.decode()
             print('Taking MSG ', data)
         except:
@@ -30,16 +37,18 @@ while True:
 
     #process data
     #send msg on client
-    for sock in users_socket:
+    for user in users:
         try:
-            sock.send('GAT #19'.encode())
+            user.conn.send('GATE $19'.encode())
+            user.err=0
         except:
-            users_socket.remove(sock)
-            sock.close()
-            print('Disconnect user')
-        
+            user.err+=1
 
-            
+    #clear list users
+    for user in users:
+        if user.err == 10:
+            user.conn.close()
+            users.remove(user)            
 
 
     #data base users
